b3a2698237f5fb6a1394ff0c95cd0e783ef3ab22
Onwukwennamdi <nnamdionwukwe@gmail.com>
2026-03-23 21:04:33 +0100

    eve

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import app from "./src/app.js";
 import { createServer } from "http";
 import { initSocket } from "./src/socket/index.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const httpServer = createServer(app);
 
 initSocket(httpServer);
