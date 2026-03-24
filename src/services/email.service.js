8ffe9ffe94922cada7dd0ed22e018a2b409d4a54
Onwukwennamdi <nnamdionwukwe@gmail.com>
2026-03-24 17:29:49 +0100

    v4

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -12,6 +12,17 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+// const transporter = nodemailer.createTransport({
+//   host: process.env.MAIL_HOST,
+//   port: 587,
+//   secure: false,
+//   requireTLS: true,
+//   auth: {
+//     user: process.env.MAIL_USER,
+//     pass: process.env.MAIL_PASS,
+//   },
+// });
+
 // Verify connection on startup
 transporter.verify((error) => {
   if (error) {
